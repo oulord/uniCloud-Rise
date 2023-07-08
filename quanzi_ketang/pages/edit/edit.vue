89b92d4bbc9c1536e475075f489c8864a12fe3f0
guo <2246338405@qq.com>
2023-07-08 20:02:58 +0800

    项目第五次提交
    自定义富文本编辑器editor

---
+++ quanzi_ketang/pages/edit/edit.vue
@@ -0,0 +1,85 @@
+<template>
+	<view class="edit">
+		<view class="title">
+			<input type="text" placeholder="请输入标题" placeholder-class="placeholderClass">
+		</view>
+
+		<view class="content">
+			<editor placeholder="写点什么吧..."></editor>
+		</view>
+
+		<view class="btnGroup">
+			<u-button type="primary" text="确定发表" disabled=""></u-button>
+		</view>
+
+		<view class="tools">
+			<view class="item"><text class="iconfont icon-zitibiaoti"></text></view>
+			<view class="item"><text class="iconfont icon-zitijiacu"></text></view>
+			<view class="item"><text class="iconfont icon-zitixieti"></text></view>
+			<view class="item"><text class="iconfont icon-fengexian"></text></view>
+			<view class="item"><text class="iconfont icon-charutupian"></text></view>
+			<view class="item"><text class="iconfont icon-duigou_kuai"></text></view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	/deep/ .ql-blank::before{
+		font-style: normal;
+		color: #e0e0e0;
+	}
+	.edit {
+		padding: 30rpx;
+
+		.title {
+			input {
+				height: 120rpx;
+				font-size: 46rpx;
+				border-bottom: 1px solid #e4e4e4;
+				margin-bottom: 30rpx;
+				color: #000;
+			}
+
+			.placeholderClass {
+				color: #e0e0e0;
+			}
+		}
+
+		.content {
+			height: calc(100vh-500rpx);
+			margin-bottom: 30rpx;
+		}
+
+		.tools {
+			width: 100%;
+			height: 80rpx;
+			background: #fff;
+			border-top: 1rpx solid #f4f4f4;
+			position: fixed;
+			left: 0;
+			bottom: 0;
+			display: flex;
+			justify-content: space-around;
+			align-items: center;
+
+			.iconfont {
+				font-size: 36rpx;
+				color: #333;
+
+				&:active {
+					color: #0199FE
+				}
+			}
+		}
+	}
+</style>
