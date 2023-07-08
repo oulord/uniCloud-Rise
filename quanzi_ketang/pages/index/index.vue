ce801649d40de5ee7da313275dd320af1437645c
guo <2246338405@qq.com>
2023-07-08 18:21:25 +0800

    项目第三次提交
    uview组件的tabs标签和Skeleton骨架屏的使用

--- quanzi_ketang/pages/index/index.vue
+++ quanzi_ketang/pages/index/index.vue
@@ -1,7 +1,28 @@
 <template>
 	<view class="home">
-		首页
-		<text class="iconfont icon-collection-fill"></text>
+		<view class="topnav">
+			<u-tabs :list="list1" @click="click" :activeStyle="{
+			    color: '#303133',
+			    fontWeight: 'bold',
+			    transform: 'scale(1.05)'
+			}" :inactiveStyle="{
+			            color: '#606266',
+			            transform: 'scale(1)'
+			}"></u-tabs>
+		</view>
+
+		<!-- 骨架屏 -->
+		<view class="loadingState" v-show="loadState">
+			<u-skeleton rows="4" title loading v-show="loadState"></u-skeleton>
+		</view>
+
+		<view class="content">
+			主体
+		</view>
+
+		<view class="edit">
+			<text class="iconfont icon-a-21-xiugai"></text>
+		</view>
 	</view>
 </template>
 
@@ -9,7 +30,12 @@
 	export default {
 		data() {
 			return {
-				
+				list1: [{
+					name: "最新",
+				}, {
+					name: "热门"
+				}],
+				loadState: false
 			}
 		},
 		onLoad() {
@@ -22,5 +48,29 @@
 </script>
 
 <style lang="scss" scoped>
-	
+	.home {
+		.topnav {
+			margin-bottom: 30rpx;
+		}
+
+		.edit {
+			width: 120rpx;
+			height: 120rpx;
+			background: #0199FE;
+			border-radius: 50%;
+			color: #fff;
+			position: fixed;
+			z-index: 100;
+			bottom: 150rpx;
+			right: 50rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			box-shadow: 0 0 20rpx rgba(1, 153, 254, 0.8);
+
+			.iconfont {
+				font-size: 50rpx;
+			}
+		}
+	}
 </style>
