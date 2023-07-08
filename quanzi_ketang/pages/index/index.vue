f6d9b416f16c3efc7607fd8b9facd3c5bf159959
guo <2246338405@qq.com>
2023-07-08 16:57:57 +0800

    项目第一次提交
    全局配置及导航切换

---
+++ quanzi_ketang/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="home">
+		首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
